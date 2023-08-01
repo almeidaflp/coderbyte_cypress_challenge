1b0f34c79e8350bfcd0de07bbda30ed8ad90d802
Felipe Almeida <f.almeida@dasa.com.br>
2023-08-01 17:34:54 -0300

    Checkout locators adding

---
+++ cypress/support/pages/checkout/checkout-locators.js
@@ -0,0 +1,12 @@
+const ELEMENTS = {
+    placeOrderButton: '.col-lg-1 > .btn',
+    orderName: '#name',
+    orderCountry: '#country',
+    orderCity: '#city',
+    orderCreditCard: '#card',
+    orderCardMonth: '#month',
+    purchaseButton:
+        '#orderModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary',
+};
+
+export default ELEMENTS;
